f3b7a466cbb972fd48cf85d7df254db0cbc27875
AnnaNatalchishina <anna.natalchihshina@gmail.com>
2022-03-21 20:49:15 +0200

    add lesson_2.py add lesson_3.py

--- lesson_1.py
+++ lesson_1.py
@@ -1,2 +1,69 @@
-print("Hello")
-print("Hello World")
+# # Ctrl + ? - закоментировать строку
+# # переменная
+#
+# value = 3
+# new_value = 5.1
+# my_str = "Hello!!!"
+#
+# print(value, new_value, my_str, print)
+#
+# # int - тип целых чисел <class int>
+# # type - получение типа объекта
+# print(type(value))
+#
+# val_1 = 23
+# val_2 = 10
+# # result = val + val_2
+# # result = val * val_2
+# # result = val - val_2
+# # result = val / val_2
+# result = val_1 // val_2 # целочисленное деление (целая часть числа в математическом смысле)
+# result = val_1 % val_2 # остаток от деления
+# print(result, type(result))
+# print(result, type(result))
+#
+# val_1 = 28.8
+# val_2 = 10.4
+# # result = val + val_2
+# # result = val * val_2
+# # result = val - val_2
+# # result = val / val_2
+# result = val_1 // val_2 # целочисленное деление (целая часть числа в математическом смысле)
+# result = val_1 % val_2 # остаток от деления
+#
+# # my_str = "New_string"
+# # my_str = 'New_string'
+# # my_str = """New_string"""
+# my_str = 'FC "Arsenal"'
+# print(my_str, type(my_str))
+#
+# new_str = r'My\tname\tis\tVova' # raw string
+#
+# f_str = f'My value is {value}'
+# print(f_str)
+#
+# my_int = 5
+# my_float = 3.14
+# my_str = "New_string"
+# new_str = "ing"
+#
+# result = my_str + my_str
+#
+# print(result)
+#
+# folder = "images"
+# filename = "photo_1.png"
+# # path = folder + "/" +
+#
+# print(1 + 2 + 3)
+#
+# value = 1 * 2 * 3
+# print(value)
+#
+value_1 = "2"
+
+value_2 = "4"
+
+result = value_1 + value_2
+
+print(result)
